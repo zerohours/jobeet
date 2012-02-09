abeb0b966f311747f5ae88f81056c17bd34cdf57
Julio César Brizuela <brizuelaalvarado@gmail.com>
2012-02-08 20:32:27 -0600

    Symfony - Septimo día

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -36,6 +36,27 @@ class JobeetJobTable extends Doctrine_Table
         return $q->execute();
     }
 
+    public function countActiveJobs(Doctrine_Query $q = null)
+    {
+        return $this->addActiveJobsQuery($q)->count();
+    }
+
+    public function addActiveJobsQuery(Doctrine_Query $q = null)
+    {
+        if (is_null($q))
+        {
+            $q = Doctrine_Query::create()
+               ->from('JobeetJob j');
+        }
+
+        $alias = $q->getRootAlias();
+
+        $q->andWhere($alias . '.expires_at > ?', date('Y-m-d h:i:s', time()))
+          ->addOrderBy($alias . '.expires_at DESC');
+
+        return $q;
+    }
+
     public function getWithJobs()
     {
         $q = $this->createQuery('c')
